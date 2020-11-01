32975707e659c5718606cedb35587836185accd9
Andrea Rus <andrearus27@gmail.com>
2020-11-01 15:26:55 +0000

    Update scripts.js

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -57,3 +57,4 @@ ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
