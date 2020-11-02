934f64b432328919ae534f2269d3651fb2489175
Andrea Rus <andrearus27@gmail.com>
2020-11-02 16:11:02 +0000

    Update script.js

--- FFC Daily Practise/script.js
+++ FFC Daily Practise/script.js
@@ -1,8 +1,26 @@
 const log = console.log;
 
+//for loop
+
 myArray = [1, 2, 4, 7, 9];
 total = 0;
 
 for(var i = 0; i < myArray.length; i++) {
   log(total += myArray[i]);
 }
+
+//nesting for loops
+
+function multiplyAll(array) {
+  var product = 1;
+
+  for(var i = 0; i < array.length; i++) {
+    for(var j = 0; j < array[i].length; j++) {
+      product *= array[i][j];
+    }
+  }
+
+  return product;
+}
+
+var product = multiplyAll([[1,2],[3,4],[5,6,7]]);
