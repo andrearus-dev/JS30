31dffffc52d8fa7a79b9321524fac1eda6e2750c
Andrea Rus <andrearus27@gmail.com>
2021-02-23 13:48:13 +0000

    Update script.js

--- FFC Daily Practise/script.js
+++ FFC Daily Practise/script.js
@@ -32,8 +32,9 @@ log(product);
 function factorial(n) {
   if (n == 1 || n == 0) {
     return 1;
+  } else 
   return n * factorial(n - 1) 
 }
 
-console.log(factorial(4))
+console.log(factorial(4));
 
