f6e783a07e0309a8909086ecd8d8c0cde8c29954
Andrea Rus <andrearus27@gmail.com>
2021-02-24 12:51:55 +0000

    Update script.js

--- FFC Daily Practise/script.js
+++ FFC Daily Practise/script.js
@@ -42,8 +42,16 @@ function sum(arr, n) {
   if(n <= 0) { 
     return 0;
   } else {
-    return sum(arr, n - 1);
+    return sum(arr, n - 1) + arr[n - 1];
   }
 }
 
-console.log
+function multiply(arr, n) {
+  var product = 1;
+  for (var i = 0; i < n; i++) {
+    product *= arr[i];
+  }
+  return product;
+}
+
+
